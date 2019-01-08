7161e09aace804a69c28a311383b6de31463f4c0
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2019-01-08 18:54:36 +0100

    Add missing class

---
+++ src/main/kotlin/helper/YamlUtils.kt
@@ -0,0 +1,16 @@
+package helper
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+
+/**
+ * Utility functions to manipulate YAML objects and arrays
+ * @author Michel Kraemer
+ */
+object YamlUtils {
+  val mapper: ObjectMapper = ObjectMapper(YAMLFactory())
+      .registerKotlinModule()
+      .setSerializationInclusion(JsonInclude.Include.NON_NULL)
+}
