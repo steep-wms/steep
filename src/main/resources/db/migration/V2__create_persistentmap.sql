8144f3809dfaa5ad14c83cc7da410afe938159f4
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2019-03-22 10:20:31 +0100

    Remove persistent collections again
    They didn't work out as expected

--- src/main/resources/db/migration/V2__create_persistentmap.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE persistentmap (
-  name VARCHAR,
-  k VARCHAR,
-  v VARCHAR,
-  CONSTRAINT persistentmap_name_k PRIMARY KEY(name, k)
-);
--- src/test/kotlin/db/PostgreSQLSubmissionRegistryTest.kt
+++ src/test/kotlin/db/PostgreSQLSubmissionRegistryTest.kt
@@ -1,9 +1,17 @@
 package db
 
 import io.vertx.core.Vertx
+import io.vertx.ext.jdbc.JDBCClient
 import io.vertx.junit5.VertxTestContext
+import io.vertx.kotlin.core.json.json
+import io.vertx.kotlin.core.json.obj
+import io.vertx.kotlin.coroutines.dispatcher
+import io.vertx.kotlin.ext.sql.updateAwait
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.AfterEach
+import org.testcontainers.jdbc.ContainerDatabaseDriver
 
 /**
  * Tests for the [PostgreSQLSubmissionRegistry]
@@ -11,16 +19,26 @@ import org.junit.jupiter.api.AfterEach
  */
 class PostgreSQLSubmissionRegistryTest : SubmissionRegistryTest() {
   companion object {
+    /**
+     * Use Testcontainers' feature to automatically create a container for a
+     * JDBC database if you add the tc: prefix to the URL. Use TC_DAEMON=true
+     * to keep the container running until all tests have finished
+     */
+    const val URL = "jdbc:tc:postgresql:10.5://hostname/databasename?TC_DAEMON=true"
+
+    /**
+     * Kill the test container automatically created by Testcontainers as soon
+     * as all tests have finished
+     */
     @AfterAll
     @JvmStatic
     fun shutdown() {
-      PostgreSQLTestUtils.shutdown()
+      ContainerDatabaseDriver.killContainer(URL)
     }
   }
 
   override fun createRegistry(vertx: Vertx): SubmissionRegistry {
-    return PostgreSQLSubmissionRegistry(vertx, PostgreSQLTestUtils.URL,
-        "user", "password")
+    return PostgreSQLSubmissionRegistry(vertx, URL, "user", "password")
   }
 
   /**
@@ -28,6 +46,19 @@ class PostgreSQLSubmissionRegistryTest : SubmissionRegistryTest() {
    */
   @AfterEach
   fun clearDatabase(vertx: Vertx, ctx: VertxTestContext) {
-    PostgreSQLTestUtils.clearDatabase(vertx, ctx)
+    val jdbcConfig = json {
+      obj(
+          "url" to URL,
+          "user" to "user",
+          "password" to "password"
+      )
+    }
+    val client = JDBCClient.createShared(vertx, jdbcConfig)
+
+    GlobalScope.launch(vertx.dispatcher()) {
+      client.updateAwait("DELETE FROM submissions")
+      client.updateAwait("DELETE FROM processchains")
+      ctx.completeNow()
+    }
   }
 }
