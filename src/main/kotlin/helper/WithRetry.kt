1f8a5a9f6c0158ebbad7faba63b077a5262e17e0
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2020-10-17 11:34:08 +0200

    Log failed attempts when retrying

--- src/main/kotlin/helper/WithRetry.kt
+++ src/main/kotlin/helper/WithRetry.kt
@@ -2,8 +2,11 @@ package helper
 
 import kotlinx.coroutines.delay
 import model.retry.RetryPolicy
+import org.slf4j.LoggerFactory
 import kotlin.math.min
 import kotlin.math.pow
+
+private val log = LoggerFactory.getLogger("withRetry")
 
 /**
  * Execute the given [block] with a retry [policy] (i.e. retry the execution
@@ -20,6 +23,15 @@ suspend fun <R> withRetry(policy: RetryPolicy?, block: suspend () -> R): R? {
   var attempt = 0
   var lastThrowable: Throwable? = null
   while (policy.maxAttempts == -1 || attempt < policy.maxAttempts) {
+    val failedMsg = when {
+      policy.maxAttempts > 0 ->
+        "Operation failed $attempt out of ${policy.maxAttempts} times."
+      attempt > 1 ->
+        "Operation failed $attempt times."
+      else ->
+        "Operation failed $attempt time."
+    }
+
     if (attempt > 0 && policy.delay > 0) {
       var actualDelay = (policy.delay * policy.exponentialBackoff.toDouble().pow(attempt - 1))
           .coerceAtMost(Long.MAX_VALUE.toDouble())
@@ -27,7 +39,18 @@ suspend fun <R> withRetry(policy: RetryPolicy?, block: suspend () -> R): R? {
       if (policy.maxDelay != null) {
         actualDelay = min(actualDelay, policy.maxDelay)
       }
+
+      if (lastThrowable != null) {
+        log.error("$failedMsg Retrying after $actualDelay milliseconds.")
+        log.trace("Cause", lastThrowable)
+      }
+
       delay(actualDelay)
+    } else {
+      if (lastThrowable != null) {
+        log.error("$failedMsg Retrying now ...")
+        log.trace("Cause", lastThrowable)
+      }
     }
 
     try {
