a0e2a4d4058f94f9a509294e3d96f04023229cf3
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2021-02-16 14:33:25 +0100

    Update testcontainers to fix CI build

--- build.gradle.kts
+++ build.gradle.kts
@@ -78,8 +78,8 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.0")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.7.0")
-    testImplementation("org.testcontainers:testcontainers:1.15.0")
-    testImplementation("org.testcontainers:postgresql:1.15.0")
+    testImplementation("org.testcontainers:testcontainers:1.15.2")
+    testImplementation("org.testcontainers:postgresql:1.15.2")
 }
 
 application {
