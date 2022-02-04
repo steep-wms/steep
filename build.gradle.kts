0ec70d8a6afbe4ba0080a1c8091bcaf9c7045be5
Michel Krämer <michel.kraemer@igd.fraunhofer.de>
2022-02-04 10:46:31 +0100

    Combined dependencies PR (#138)
    * Bump postcss from 8.4.5 to 8.4.6 in /ui
    
    Bumps [postcss](https://github.com/postcss/postcss) from 8.4.5 to 8.4.6.
    - [Release notes](https://github.com/postcss/postcss/releases)
    - [Changelog](https://github.com/postcss/postcss/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/postcss/postcss/compare/8.4.5...8.4.6)
    
    ---
    updated-dependencies:
    - dependency-name: postcss
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump sass from 1.49.4 to 1.49.7 in /ui
    
    Bumps [sass](https://github.com/sass/dart-sass) from 1.49.4 to 1.49.7.
    - [Release notes](https://github.com/sass/dart-sass/releases)
    - [Changelog](https://github.com/sass/dart-sass/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/sass/dart-sass/compare/1.49.4...1.49.7)
    
    ---
    updated-dependencies:
    - dependency-name: sass
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump postgresql from 42.3.1 to 42.3.2
    
    Bumps [postgresql](https://github.com/pgjdbc/pgjdbc) from 42.3.1 to 42.3.2.
    - [Release notes](https://github.com/pgjdbc/pgjdbc/releases)
    - [Changelog](https://github.com/pgjdbc/pgjdbc/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/pgjdbc/pgjdbc/compare/REL42.3.1...REL42.3.2)
    
    ---
    updated-dependencies:
    - dependency-name: org.postgresql:postgresql
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump clipboard from 2.0.9 to 2.0.10 in /ui
    
    Bumps [clipboard](https://github.com/zenorocha/clipboard.js) from 2.0.9 to 2.0.10.
    - [Release notes](https://github.com/zenorocha/clipboard.js/releases)
    - [Commits](https://github.com/zenorocha/clipboard.js/compare/v2.0.9...v2.0.10)
    
    ---
    updated-dependencies:
    - dependency-name: clipboard
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump flyway-core from 8.4.3 to 8.4.4
    
    Bumps [flyway-core](https://github.com/flyway/flyway) from 8.4.3 to 8.4.4.
    - [Release notes](https://github.com/flyway/flyway/releases)
    - [Commits](https://github.com/flyway/flyway/compare/flyway-8.4.3...flyway-8.4.4)
    
    ---
    updated-dependencies:
    - dependency-name: org.flywaydb:flyway-core
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -66,10 +66,10 @@ dependencies {
     implementation("org.apache.ant:ant:1.10.12")
     implementation("org.apache.commons:commons-lang3:3.12.0")
     implementation("org.apache.commons:commons-text:1.9")
-    implementation("org.flywaydb:flyway-core:8.4.3")
+    implementation("org.flywaydb:flyway-core:8.4.4")
     implementation("org.mongodb:mongodb-driver-reactivestreams:4.4.1")
     implementation("com.github.openstack4j.core:openstack4j:3.10")
-    implementation("org.postgresql:postgresql:42.3.1")
+    implementation("org.postgresql:postgresql:42.3.2")
     implementation("org.quartz-scheduler:quartz:2.3.2") {
         // we only need org.quartz.CronExpression, so we can exclude all dependencies
         isTransitive = false
