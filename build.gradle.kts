94e9158cb09cb939018ca20963eb7ed373e8fc24
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2022-03-14 14:45:08 +0100

    Improve Hazelcast performance

--- build.gradle.kts
+++ build.gradle.kts
@@ -94,6 +94,17 @@ dependencies {
 
 application {
     mainClassName = "MainKt"
+
+    applicationDefaultJvmArgs = listOf(
+        // required to improve performance of Hazelcast
+        "--add-modules", "java.se",
+        "--add-exports", "java.base/jdk.internal.ref=ALL-UNNAMED",
+        "--add-opens", "java.base/java.lang=ALL-UNNAMED",
+        "--add-opens", "java.base/java.nio=ALL-UNNAMED",
+        "--add-opens", "java.base/sun.nio.ch=ALL-UNNAMED",
+        "--add-opens", "java.management/sun.management=ALL-UNNAMED",
+        "--add-opens", "jdk.management/com.sun.management.internal=ALL-UNNAMED"
+    )
 }
 
 jacoco {
