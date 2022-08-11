306e8d9041e1521fe284221f0068fc3599c9a7dd
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2022-08-11 13:00:47 +0200

    Make code box scalable

--- ui/components/CodeBox.jsx
+++ ui/components/CodeBox.jsx
@@ -1,54 +1,135 @@
 import classNames from "classnames"
 import styles from "./CodeBox.scss"
 import Tooltip from "./Tooltip"
-import { useEffect, useRef, useState } from "react"
+import React, { forwardRef, memo, useEffect, useRef, useState } from "react"
 import stringify from "./lib/yaml-stringify"
 import Clipboard from "clipboard"
 import { Clipboard as ClipboardIcon } from "react-feather"
+import { toH } from "hast-to-hyperscript"
+import { VariableSizeList, areEqual } from "react-window"
+import { useRouter } from "next/router"
 
 const COPY = "Copy to clipboard"
 const COPIED = "Copied!"
+const BLOCK_SIZE = 1000
+
+const Row = memo(({ data, index, style }) => {
+  let item = data[index]
+  let block = item.block
+  if (typeof block === "object") {
+    block = toH(React.createElement, block)
+  }
+  return <div style={style}>{block}</div>
+}, areEqual)
+
+const List = forwardRef(({ language, codeLineHeight, blocks }, ref) => {
+  return <pre><code lang={language} className={`hljs language-${language}`} ref={ref}><VariableSizeList
+    height={codeLineHeight * Math.min(blocks.blocks[0].lines, 100.5)}
+    itemCount={blocks.blocks.length} itemData={blocks.blocks}
+    estimatedItemSize={codeLineHeight * BLOCK_SIZE} width="100%"
+    itemSize={(index) => codeLineHeight * blocks.blocks[index].lines}>{Row}</VariableSizeList></code></pre>
+})
+
+function splitIntoBlocks(str) {
+  let lines = 0
+  let blocks = []
+  let i = -1
+  let li = 0
+  do {
+    i = str.indexOf("\n", i + 1)
+    if (i >= 0) {
+      lines++
+      if (lines === BLOCK_SIZE) {
+        blocks.push({
+          block: str.substring(li, i + 1),
+          lines: lines
+        })
+        li = i + 1
+        lines = 0
+      }
+    } else {
+      if (li < str.length) {
+        lines++
+        blocks.push({
+          block: str.substring(li, str.length),
+          lines
+        })
+      }
+    }
+  } while (i >= 0)
+
+  return blocks
+}
 
 const CodeBox = ({ json, yaml = undefined }) => {
   const jsonRef = useRef()
   const yamlRef = useRef()
-  const yamlStrRef = useRef()
   const copyBtnRef = useRef()
-  const [highlightedJson, setHighlightedJson] = useState()
-  const [highlightedYaml, setHighlightedYaml] = useState()
+  const [yamlBlocks, setYamlBlocks] = useState(() => {
+    // this should only be done once, so we do it in a state initializer
+    let str = yaml?.trim() || stringify(json)
+    let blocks = splitIntoBlocks(str)
+    return {
+      str,
+      blocks
+    }
+  })
+  const [jsonBlocks, setJsonBlocks] = useState(() => {
+    // this should only be done once, so we do it in a state initializer
+    let str = JSON.stringify(json, undefined, 2)
+    let blocks = splitIntoBlocks(str)
+    return {
+      str,
+      blocks
+    }
+  })
   const [copyTooltipVisible, setCopyTooltipVisible] = useState(false)
   const [copyTooltipTitle, setCopyTooltipTitle] = useState(COPY)
   const [activeLang, setActiveLang] = useState(localStorage.activeCodeLanguage || "yaml")
-  const [highlightingTooLongJson, setHighlightingTooLongJson] = useState(false)
-  const [highlightingTooLongYaml, setHighlightingTooLongYaml] = useState(false)
+  const [codeLineHeight, setCodeLineHeight] = useState(28)
+  const router = useRouter()
 
-  let str = JSON.stringify(json, undefined, 2)
-  if (yamlStrRef.current === undefined) {
-    yamlStrRef.current = yaml || stringify(json)
-  }
-  let yamlStr = yamlStrRef.current
-
-  function highlight(code, language, success, error) {
-    let finished = false
+  function highlight(code, language, updateBlocks) {
     let worker = new Worker(new URL("./lib/highlight-worker.js", import.meta.url))
-    worker.onmessage = ({ data: { html } }) => {
-      finished = true
-      success(html)
-    }
-    worker.postMessage({ code, language })
-
-    setTimeout(() => {
-      if (!finished) {
-        error()
-        worker.terminate()
+    let count = 0
+    worker.onmessage = ({ data: { hast, finished } }) => {
+      if (hast !== undefined) {
+        updateBlocks((state) => {
+          let newBlocks = [...state.blocks]
+          newBlocks[count] = { ...newBlocks[count], block: hast }
+          ++count
+          return {
+            str: state.str,
+            blocks: newBlocks
+          }
+        })
       }
-    }, 2000)
+    }
+    worker.postMessage({ code, language, blockSize: BLOCK_SIZE })
+
+    return worker
   }
 
   useEffect(() => {
-    highlight(yamlStr, "yaml", setHighlightedYaml, () => setHighlightingTooLongYaml(true))
-    highlight(str, "json", setHighlightedJson, () => setHighlightingTooLongJson(true))
-  }, [str, yamlStr])
+    setCodeLineHeight(parseFloat(getComputedStyle(yamlRef.current).lineHeight))
+  }, [])
+
+  useEffect(() => {
+    let yamlWorker = highlight(yamlBlocks.str, "yaml", setYamlBlocks)
+    let jsonWorker = highlight(jsonBlocks.str, "json", setJsonBlocks)
+
+    function handleRouteChange() {
+      // abort highlighting if user wants to visit another page
+      yamlWorker.terminate()
+      jsonWorker.terminate()
+    }
+
+    router.events.on("routeChangeStart", handleRouteChange)
+
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChange)
+    }
+  }, [yamlBlocks.str, jsonBlocks.str, router])
 
   useEffect(() => {
     let clipboardYaml = new Clipboard(copyBtnRef.current, {
@@ -81,29 +162,15 @@ const CodeBox = ({ json, yaml = undefined }) => {
           onClick={() => onClickLanguage("yaml")}>YAML</div>
         <div className={classNames("code-box-title-tab", { active: activeLang === "json" })}
           onClick={() => onClickLanguage("json")}>JSON</div>
-        {highlightingTooLongJson && activeLang === "json" && <div className="highlighting-disabled">
-          Syntax highlighting was disabled because it took too long
-        </div>}
-        {highlightingTooLongYaml && activeLang === "yaml" && <div className="highlighting-disabled">
-          Syntax highlighting was disabled because it took too long
-        </div>}
       </div>
       <div className="code-box-main">
         <div className={classNames("code-box-tab", { active: activeLang === "yaml" })}>
-          {highlightedYaml && (
-            <pre><code lang="json" className="hljs language-yaml" ref={yamlRef}
-              dangerouslySetInnerHTML={{ __html: highlightedYaml }}></code></pre>
-          ) || (
-            <pre><code lang="json" className="hljs language-yaml" ref={yamlRef}>{yamlStr}</code></pre>
-          )}
+          <List language="yaml" codeLineHeight={codeLineHeight} blocks={yamlBlocks}
+            ref={yamlRef} />
         </div>
         <div className={classNames("code-box-tab", { active: activeLang === "json" })}>
-          {highlightedJson && (
-            <pre><code lang="json" className="hljs language-json" ref={jsonRef}
-              dangerouslySetInnerHTML={{ __html: highlightedJson }}></code></pre>
-          ) || (
-            <pre><code lang="json" className="hljs language-json" ref={jsonRef}>{str}</code></pre>
-          )}
+          <List language="json" codeLineHeight={codeLineHeight} blocks={jsonBlocks}
+            ref={jsonRef} />
         </div>
         <span className="code-box-copy-btn">
           <Tooltip title={copyTooltipTitle} forceVisible={copyTooltipVisible}
--- ui/components/lib/highlight-worker.js
+++ ui/components/lib/highlight-worker.js
@@ -1,10 +1,160 @@
 import hljs from "highlight.js/lib/core"
 import json from "highlight.js/lib/languages/json"
 import yaml from "highlight.js/lib/languages/yaml"
+
+/**
+ * Based on HastEmitter from lowlight (https://github.com/wooorm/lowlight) but
+ * modified to issue blocks of highlighted code to the caller of this worker.
+ * The original code is released under the MIT license by Titus Wormer
+ */
+class HastEmitter {
+  constructor(options) {
+    this.options = options
+    this.root = {
+      type: "element",
+      tagName: "span",
+      children: []
+    }
+    this.stack = [this.root]
+    this.lineCount = 0
+  }
+
+  clearStack(stack) {
+    let newStack = []
+    let o = stack[stack.length - 1]
+    if (o.children !== undefined) {
+      o = { ...o, children: [] }
+    }
+    newStack.push(o)
+    for (let i = stack.length - 2; i >= 0; --i) {
+      let newParent = { ...stack[i], children: [o] }
+      newStack.push(newParent)
+      o = newParent
+    }
+    return newStack
+  }
+
+  addText(value) {
+    if (value.length === 0) {
+      return
+    }
+
+    let current = this.stack[this.stack.length - 1]
+    let tail = current.children[current.children.length - 1]
+
+    let rest
+    let lf = -1
+    do {
+      lf = value.indexOf("\n", lf + 1)
+      if (lf >= 0) {
+        this.lineCount++
+        if (this.lineCount === self.blockSize) {
+          rest = value.substring(lf + 1)
+          value = value.substring(0, lf + 1)
+          this.lineCount = 0
+          break
+        }
+      }
+    } while (lf >= 0)
+
+    if (tail && tail.type === "text") {
+      tail.value += value
+    } else {
+      current.children.push({
+        type: "text",
+        value
+      })
+    }
+
+    if (rest !== undefined) {
+      self.postMessage({
+        hast: this.stack[0]
+      })
+      this.stack = this.clearStack(this.stack)
+
+      let current = this.stack[this.stack.length - 1]
+      if (rest.length > 0) {
+        current.children.push({
+          type: "text",
+          value: rest
+        })
+      }
+    }
+  }
+
+  addKeyword(value, name) {
+    this.openNode(name)
+    this.addText(value)
+    this.closeNode()
+  }
+
+  addSublanguage(other, name) {
+    let current = this.stack[this.stack.length - 1]
+    let results = other.root.children
+
+    if (name) {
+      current.children.push({
+        type: "element",
+        tagName: "span",
+        properties: {
+          className: [name]
+        },
+        children: results
+      })
+    } else {
+      current.children.push(...results)
+    }
+  }
+
+  openNode(name) {
+    // First “class” gets the prefix. Rest gets a repeated underscore suffix.
+    // See: <https://github.com/highlightjs/highlight.js/commit/51806aa>
+    // See: <https://github.com/wooorm/lowlight/issues/43>
+    let className = name
+      .split(".")
+      .map((d, i) => (i ? d + "_".repeat(i) : this.options.classPrefix + d))
+
+    let current = this.stack[this.stack.length - 1]
+    let child = {
+      type: "element",
+      tagName: "span",
+      properties: {
+        className
+      },
+      children: []
+    }
+
+    current.children.push(child)
+    this.stack.push(child)
+  }
+
+  closeNode() {
+    this.stack.pop()
+  }
+
+  closeAllNodes() {
+    // nothing to do here
+  }
+
+  finalize() {
+    self.postMessage({
+      hast: this.stack[0]
+    })
+  }
+
+  toHTML() {
+    return ""
+  }
+}
+
+hljs.configure({ __emitter: HastEmitter })
 hljs.registerLanguage("json", json)
 hljs.registerLanguage("yaml", yaml)
 
-self.onmessage = ({ data: { code, language } }) => {
-  let html = hljs.highlight(code, { language }).value
-  self.postMessage({ html })
+self.onmessage = ({ data: { code, language, blockSize } }) => {
+  self.blockSize = blockSize
+  hljs.highlight(code, { language }).value
+  self.postMessage({
+    finished: true
+  })
 }
