6c64ba9be57bff2a382df03eba5ede57df4f0068
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2024-04-16 14:02:41 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,12 @@
 import java.text.SimpleDateFormat
 import java.util.Date
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     application
     jacoco
-    kotlin("jvm") version "1.9.22"
-    id("com.github.ben-manes.versions") version "0.50.0"
+    kotlin("jvm") version "1.9.23"
+    id("com.github.ben-manes.versions") version "0.51.0"
     id("se.patrikerdes.use-latest-versions") version "0.2.18"
 }
 
@@ -20,60 +19,54 @@ buildscript {
 group = "de.fhg.igd"
 version = "6.9.0-SNAPSHOT"
 
-val prometheusClientVersion by extra("0.16.0")
-val slf4jVersion by extra("2.0.9")
-val logbackVersion by extra("1.4.11")
-val junitVersion by extra("5.10.0")
-val testcontainersVersion by extra("1.19.0")
-
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.slf4j:jul-to-slf4j:$slf4jVersion")
-    implementation("org.slf4j:log4j-over-slf4j:$slf4jVersion")
-    implementation("org.slf4j:slf4j-api:$slf4jVersion")
+    implementation("org.slf4j:jul-to-slf4j:2.0.13")
+    implementation("org.slf4j:log4j-over-slf4j:2.0.13")
+    implementation("org.slf4j:slf4j-api:2.0.13")
 
-    implementation("ch.qos.logback:logback-classic:$logbackVersion")
-    implementation("ch.qos.logback:logback-core:$logbackVersion")
+    implementation("ch.qos.logback:logback-classic:1.5.5")
+    implementation("ch.qos.logback:logback-core:1.5.5")
     implementation("ch.qos.logback.contrib:logback-jackson:0.1.5")
     implementation("ch.qos.logback.contrib:logback-json-classic:0.1.5")
-    implementation("org.codehaus.janino:janino:3.1.11") // for conditionals in logback.xml
+    implementation("org.codehaus.janino:janino:3.1.12") // for conditionals in logback.xml
 
-    implementation("io.vertx:vertx-core:4.5.3")
-    implementation("io.vertx:vertx-hazelcast:4.5.3")
-    implementation("io.vertx:vertx-lang-kotlin:4.5.3")
-    implementation("io.vertx:vertx-lang-kotlin-coroutines:4.5.3")
-    implementation("io.vertx:vertx-micrometer-metrics:4.5.3")
-    implementation("io.vertx:vertx-mongo-client:4.5.3")
-    implementation("io.vertx:vertx-pg-client:4.5.3")
-    implementation("io.vertx:vertx-web:4.5.3")
-    implementation("io.vertx:vertx-web-client:4.5.3")
+    implementation("io.vertx:vertx-core:4.5.7")
+    implementation("io.vertx:vertx-hazelcast:4.5.7")
+    implementation("io.vertx:vertx-lang-kotlin:4.5.7")
+    implementation("io.vertx:vertx-lang-kotlin-coroutines:4.5.7")
+    implementation("io.vertx:vertx-micrometer-metrics:4.5.7")
+    implementation("io.vertx:vertx-mongo-client:4.5.7")
+    implementation("io.vertx:vertx-pg-client:4.5.7")
+    implementation("io.vertx:vertx-web:4.5.7")
+    implementation("io.vertx:vertx-web-client:4.5.7")
 
-    implementation("commons-codec:commons-codec:1.16.0")
-    implementation("commons-io:commons-io:2.15.1")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.16.1")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.1")
-    implementation("com.github.luben:zstd-jni:1.5.5-11")
-    implementation("com.github.zafarkhaja:java-semver:0.9.0")
-    implementation("com.google.guava:guava:32.1.3-jre")
+    implementation("commons-codec:commons-codec:1.16.1")
+    implementation("commons-io:commons-io:2.16.1")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.17.0")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.0")
+    implementation("com.github.luben:zstd-jni:1.5.6-2")
+    implementation("com.github.zafarkhaja:java-semver:0.10.2")
+    implementation("com.google.guava:guava:33.1.0-jre")
     implementation("com.ongres.scram:client:2.1") // support PostgreSQL SCRAM-SHA-256 authentication
     implementation("com.zaxxer:HikariCP:5.1.0")
-    implementation("io.airlift:aircompressor:0.25")
+    implementation("io.airlift:aircompressor:0.26")
     implementation("io.pebbletemplates:pebble:3.2.2")
-    implementation("io.projectreactor:reactor-core:3.6.2") // necessary for reactive MongoDB driver
-    implementation("io.prometheus:simpleclient:$prometheusClientVersion")
-    implementation("io.micrometer:micrometer-registry-prometheus:1.12.2")
+    implementation("io.projectreactor:reactor-core:3.6.5") // necessary for reactive MongoDB driver
+    implementation("io.prometheus:simpleclient:0.16.0")
+    implementation("io.micrometer:micrometer-registry-prometheus:1.12.5")
     implementation("org.apache.ant:ant:1.10.14")
     implementation("org.apache.commons:commons-lang3:3.14.0")
     implementation("org.apache.commons:commons-text:1.11.0")
     implementation("org.flywaydb:flyway-core:9.22.3")
-    implementation("org.mongodb:mongodb-driver-reactivestreams:4.11.1")
+    implementation("org.mongodb:mongodb-driver-reactivestreams:5.0.1")
     implementation("com.github.openstack4j.core:openstack4j:3.11")
     implementation("org.ow2.asm:asm:9.7") // necessary to make org.parboiled:parboiled-java compatible with Java 21
     implementation("org.parboiled:parboiled-java:1.4.1")
-    implementation("org.postgresql:postgresql:42.7.1")
+    implementation("org.postgresql:postgresql:42.7.3")
     implementation("org.quartz-scheduler:quartz:2.3.2") {
         // we only need org.quartz.CronExpression, so we can exclude all dependencies
         isTransitive = false
@@ -86,16 +79,16 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation(kotlin("script-runtime"))
 
-    testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo:4.12.0")
-    testImplementation("io.mockk:mockk:1.13.8")
-    testImplementation("io.vertx:vertx-junit5:4.5.3")
-    testImplementation("org.assertj:assertj-core:3.24.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
-    testImplementation("org.testcontainers:testcontainers:$testcontainersVersion")
-    testImplementation("org.testcontainers:junit-jupiter:$testcontainersVersion")
-    testImplementation("org.testcontainers:postgresql:$testcontainersVersion")
+    testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo:4.12.6")
+    testImplementation("io.mockk:mockk:1.13.10")
+    testImplementation("io.vertx:vertx-junit5:4.5.7")
+    testImplementation("org.assertj:assertj-core:3.25.3")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.2")
+    testImplementation("org.testcontainers:testcontainers:1.19.7")
+    testImplementation("org.testcontainers:junit-jupiter:1.19.7")
+    testImplementation("org.testcontainers:postgresql:1.19.7")
 }
 
 application {
